12f88d64eff47eeae477d761c444f649ced07a7d
Broxon <brunobartunek1@gmail.com>
2022-05-13 13:19:11 +0200

    Prvni commit

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<div class="field has-addons">
+	<p class="control">
+	  <span class="select">
+		<select>
+		  <option>$</option>
+		  <option>£</option>
+		  <option>€</option>
+		</select>
+	  </span>
+	</p>
+	<p class="control">
+	  <input class="input" type="text" placeholder="Amount of money">
+	</p>
+	<p class="control">
+	  <a href='' class="button">
+		Transfer
+	  </a>
+	</p>
+  </div>
+  
+  <div class="field has-addons">
+	<p class="control">
+	  <span class="select">
+		<select>
+		  <option>$</option>
+		  <option>£</option>
+		  <option>€</option>
+		</select>
+	  </span>
+	</p>
+	<p class="control is-expanded">
+	  <input class="input" type="text" placeholder="Amount of money">
+	</p>
+	<p class="control">
+	  <a href='' class="button">
+		Transfer
+	  </a>
+	</p>
+  </div>
