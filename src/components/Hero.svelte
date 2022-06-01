19b2fed1445f8b1823da61d4b4bf016c6f5bae89
Broxon <brunobartunek1@gmail.com>
2022-06-01 11:06:33 +0200

    Partneri

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,45 +1,206 @@
-<div id="bg-img" class="hero">
-    <div class="hero-body">
-        <div class="container">
-            <div class="columns is-multiline">
-                <div class="column is-one-third">
-                    <div class="title is-1 has-text-white-bis is-size-2-mobile">
-                        Kybernetický dron pro vzdálené hackování
-                    </div>
-                    <div
-                        class="title is-5 has-text-white-bis mt-6 is-size-6-mobile"
-                    >
-                        CyDrone disponuje širokou paletou nástrojů od rušiček
-                        rádiové komunikace až k nočnímu vidění
-                    </div>
-                    <button
-                        class="button is-active is-hovered is-medium is-rounded is-responsive is-fullwidth mt-6"
-                        >ZJISTIT VÍCE</button
-                    >
+<section class="section" id="hero">
+    <div class="container max-width">
+        <div class="columns pb-5 is-multiline">
+            <div class="column is-one-third">
+                <div class="title is-1 has-text-white-bis is-size-2-mobile">
+                    Kybernetický dron pro vzdálené hackování
                 </div>
-                <div class="column">
-                    <figure class="image">
-                        <img src="./assets/Dronik.png" alt="Drone" />
-                    </figure>
+                <div
+                    class="title is-5 has-text-white-bis mt-6 is-size-6-mobile"
+                >
+                    CyDrone disponuje širokou paletou nástrojů od rušiček
+                    rádiové komunikace až k nočnímu vidění
+                </div>
+                <button
+                    class="button is-active is-hovered is-medium is-rounded is-responsive is-fullwidth mt-6"
+                    >ZJISTIT VÍCE</button
+                >
+            </div>
+            <div class="column">
+                <figure class="image">
+                    <img src="./assets/Dronik.png" alt="Drone" />
+                </figure>
+            </div>
+        </div>
+    </div>
+</section>
+<img src="./assets/wave.svg" alt="wave" />
+
+<section class="section" id="About">
+    <div class="container">
+        <div class="has-text-centered">
+            <h2 class="title is-2">O nás</h2>
+            <h6 class="subtitle is-5 has-text-grey">
+                Představení našeho týmu, který je zodpovědný za vývoj CyDrone
+            </h6>
+        </div>
+        <div class="columns">
+            <div class="column">
+                <div class="columns">
+                    <div class="column is-3 mt-1 mr-1 ml-1">
+                        <figure class="image is-128x128">
+                            <img
+                                class="is-rounded is-128x128"
+                                src="./assets/Alex.png"
+                                alt="Alexandr Waage"
+                            />
+                        </figure>
+                    </div>
+                    <div class="column m-1 mr-0">
+                        <blockquote class="title is-4">
+                            "Myslím si, že potenciál našeho projektu je
+                            obrovský"
+                        </blockquote>
+                        <h6 class="title is-6 mb-0">Alexandr Waage</h6>
+                        <h6 class="is-6 mt-0">IoT & security engineer</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="columns">
+                    <div class="column is-3 mt-1 mr-1 ml-1">
+                        <figure class="image is-128x128">
+                            <img
+                                class="is-rounded is-128x128"
+                                src="./assets/Bruno.png"
+                                alt="Alexandr Waage"
+                            />
+                        </figure>
+                    </div>
+                    <div class="column m-1 mr-0">
+                        <blockquote class="title is-4">
+                            "Díky tomuto projektu, se můžu rozvíjet ve věcech,
+                            které mě baví"
+                        </blockquote>
+                        <h6 class="title is-6 mb-0">Bruno Bartůněk</h6>
+                        <h6 class="is-6 mt-0">Security & HW engineer</h6>
+                    </div>
                 </div>
             </div>
         </div>
-        <div />
-    </div>
-    <div id="about" class="hero-body">
-        <div class="container">
+        <div class="columns">
+            <div class="column">
+                <div class="columns">
+                    <div class="column is-3 mt-1 mr-1 ml-1">
+                        <figure class="image is-128x128">
+                            <img
+                                class="is-rounded is-128x128"
+                                src="./assets/Alex.png"
+                                alt="Alexandr Waage"
+                            />
+                        </figure>
+                    </div>
+                    <div class="column m-1 mr-0">
+                        <blockquote class="title is-4">
+                            "Naše řešení je naprosto unikátní oproti ostatním"
+                        </blockquote>
+                        <h6 class="title is-6 mb-0">Akrami Amir</h6>
+                        <h6 class="is-6 mt-0">HW engineer & IoT engineer</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="column" />
         </div>
     </div>
-</div>
+</section>
+
+<section class="section" id="Partners">
+    <div class="container">
+        <div class="has-text-centered">
+            <h2 class="title is-2">Partneři</h2>
+            <h5 class="subtitle is-5 has-text-grey">
+                Toto jsou naši partneři, bez kterých by tento projekt nemohl
+                vzniknout
+            </h5>
+        </div>
+        <div class="columns">
+            <div class="column is-3">
+                <a href="https://ssps.cz" alt="#">
+                    <img src="/assets/ssps_logo.svg" alt="logo_ssps" />
+                </a>
+            </div>
+            <div class="column">
+                <div class="box">
+                    <article class="media">
+                        <div class="media-left">
+                            <figure class="image is-64x64">
+                                <!-- svelte-ignore a11y-img-redundant-alt -->
+                                <img src="/assets/sablik.jpg" alt="Image" />
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <div class="content">
+                                <p>
+                                    <strong>Radko Sáblík</strong>
+                                    <small>
+                                        <a
+                                            href="https://twitter.com/RadkoSablik"
+                                        >
+                                            @RadkoSablik
+                                        </a></small
+                                    >
+                                    <br />
+                                    Myslím si, že projekt CyDrone vedený pod panem
+                                    Waagem je ukázkou toho, jak může fungovat podpora
+                                    středních škol mezi nadanými studenty.
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+        <div class="columns">
+            <div class="column is-3">
+                <a href="https://haxagon.cz/" alt="#">
+                    <img src="/assets/haxagon.svg" alt="logo_ssps" />
+                </a>
+            </div>
+            <div class="column">
+                <div class="box">
+                    <article class="media">
+                        <div class="media-left">
+                            <figure class="image is-64x64">
+                                <!-- svelte-ignore a11y-img-redundant-alt -->
+                                <img src="/assets/nathan.jpg" alt="Image" />
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <div class="content">
+                                <p>
+                                    <strong>Nathan Něměc</strong>
+                                    <small>
+                                        <a
+                                            href="https://twitter.com/nathannemec"
+                                        >
+                                            @NathanNemec
+                                        </a></small
+                                    >
+                                    <br />
+                                    Tento projekt mě nadchl už od počátku jeho vzniku,
+                                    doufám, že budeme nadále ve velice úzké spolupráci
+                                    se všemi členy CyDrone.
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section" id="Our_solution">
+
+</section>
 
 <style>
-
-    #bg-img {
-        background-image: url("/assets/27580.png");
-        background-position: center center;
-        background-repeat:  no-repeat;
+    #hero {
+        background-image: url("/assets/background.png");
+        background-position: center;
+        background-repeat: no-repeat;
         background-attachment: fixed;
-        background-size:  cover;
+        background-size: 100% 100%;
     }
 
     .column {
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -5,13 +5,13 @@
   }
 </script>
 
-<nav class="navbar is-spaced is-transparent is-fixed-top" id="my-nav">
+<nav class="navbar is-spaced is-transparent" id="my-nav">
     <div class="container">
         <div class="navbar-brand">
             <a href="#/" class="navbar-item">
                 <img class="cydrone" src="./assets/CyDrone.png" alt="#/" />
             </a>
-            <a href="/" class="navbar-item is-size-5 has-text-black">
+            <a href="#hero" class="navbar-item is-size-5 has-text-black">
                 CyDrone
             </a>
             <a class="navbar-burger" class:is-active={mobile} on:click={toggleNav} href="#/">
@@ -23,13 +23,13 @@
         <div class="navbar-menu" class:is-active={mobile}>
             <div class="navbar-end">
                 <div class="navbar-start">
-                    <a class="navbar-item has-text-black" href="/">
+                    <a class="navbar-item has-text-black" href="#About">
                     O nás 
                     </a>
-                    <a class="navbar-item has-text-black" href="/">
+                    <a class="navbar-item has-text-black" href="#Partners">
                     Partneři
                     </a>
-                    <a class="navbar-item has-text-black" href="/">
+                    <a class="navbar-item has-text-black" href="#Our_solution">
                     Naše řešení 
                     </a>
                     <a class="navbar-item has-text-black" href="/">
@@ -39,6 +39,7 @@
         </div>
     </div>
 </nav>
+
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@1,700&display=swap');
