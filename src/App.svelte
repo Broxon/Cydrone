1dcc69ca70017ff945f1bfacceef1d33e0a1b7b6
Broxon <brunobartunek1@gmail.com>
2022-06-10 19:47:24 +0200

    Hotovo

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script>
 	import Navbar from "./components/Navbar.svelte"
 	import Hero from "./components/Hero.svelte"
+	import Footer from "./components/Footer.svelte"
 	import 'bulma/css/bulma.css'
 	import "@fortawesome/fontawesome-free/js/all.js"
 </script>
 
 <Navbar />
 <Hero />
+<Footer />
 
 <style>
 </style>
